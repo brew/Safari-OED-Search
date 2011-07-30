fc46cbe7f8605e5a37e8734375937e8c7adb86d4
Brook Elgie <specialbrew@gmail.com>
2011-07-30 13:39:49 +0100

    Change the way auth rejections are handled.
    OED changed something that broke the previous auto-login. This version
    is much simpler and faster. It doesn't attempt an auto-login unless it
    is necessary.

--- oed-search.safariextension/inject.js
+++ oed-search.safariextension/inject.js
@@ -1,8 +1,19 @@
 document.addEventListener("contextmenu", onContextMenu, false);
 
 function onContextMenu(e) {
-    var sel = '';
-    sel += window.parent.getSelection();
-    sel = sel.replace(/^\s+|\s+$/g,"");       
-    safari.self.tab.setContextMenuEventUserInfo(e, sel);
+  var sel = '';
+  sel += window.parent.getSelection();
+  sel = sel.replace(/^\s+|\s+$/g,"");       
+  safari.self.tab.setContextMenuEventUserInfo(e, sel);
 }
+
+function messageHandler(e) {
+  if(e.name == "prefillOEDLogin") {
+    document.getElementById("libLoginCard").value = e.message.libCardNum;
+    document.getElementsByName("dest")[1].value = e.message.dest;
+    document.getElementById("libLoginForm").submit();
+  }
+}
+
+safari.self.addEventListener("message", messageHandler, false);
+safari.self.tab.dispatchMessage("tabLoadedMessage","I've loaded!");
